8fb192e5da93300f082e9e242e2ef8326964e9b1
Sapensh Taran <reandeveloper1@gmail.com>
2026-04-21 16:50:33 +0530

    link

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig,loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig((mode) => {
-    const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: './',
+    base: '/',
     build: {
       outDir: 'build',
     },
@@ -40,7 +40,7 @@ export default defineConfig((mode) => {
       ],
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
-     server: {
+    server: {
       port: parseInt(env.VITE_PORT || '3009', 10),
     },
   }
